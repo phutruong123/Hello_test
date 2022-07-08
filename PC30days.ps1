759e9f20887a660a39a86fb6b6dfcb1789879ce8
phutruong123 <trongphulk1998@gmail.com>
2022-07-08 19:02:16 +0700

    Add files via upload

---
+++ PC30days.ps1
@@ -0,0 +1,16 @@
+﻿$SearchBase = "OU=ELCA-VN,OU=Accounts,OU=VN,DC=elcaNet,DC=local"
+$AllUsers =  Get-ADUser -SearchBase $SearchBase -Filter * -Properties lastlogon
+Write-Host "Getting Users..."
+Write-Host "----------------"
+foreach($user in $AllUsers)
+    {
+        $fullname = $user.SamAccountName
+        echo $fullname
+        #$visa = $user.SamAccountName
+        #$desc = $user.Description
+        #$lastlogon = $user.lastlogondate
+        #echo $AllUsers
+        Get-ADUser -Identity $fullname -Properties LastLogon | Select Name, @{Name='LastLogon';Expression={[DateTime]::FromFileTime($_.LastLogon)}}
+        Get-Content -Path D:\Dell\OUTPUT.txt      
+                
+    }
