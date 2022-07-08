5edd227685e958d7f9bfec9c2a0a4c1daef6e4b0
phutruong123 <trongphulk1998@gmail.com>
2022-07-08 19:34:01 +0700

    Update PC30days.ps1

--- PC30days.ps1
+++ PC30days.ps1
@@ -1,16 +1,5 @@
-﻿$SearchBase = "OU=ELCA-VN,OU=Accounts,OU=VN,DC=elcaNet,DC=local"
-$AllUsers =  Get-ADUser -SearchBase $SearchBase -Filter * -Properties lastlogon
-Write-Host "Getting Users..."
-Write-Host "----------------"
-foreach($user in $AllUsers)
-    {
-        $fullname = $user.SamAccountName
-        echo $fullname
-        #$visa = $user.SamAccountName
-        #$desc = $user.Description
-        #$lastlogon = $user.lastlogondate
-        #echo $AllUsers
-        Get-ADUser -Identity $fullname -Properties LastLogon | Select Name, @{Name='LastLogon';Expression={[DateTime]::FromFileTime($_.LastLogon)}}
-        Get-Content -Path D:\Dell\OUTPUT.txt      
-                
-    }
+#!/usr/bin/pwsh -Command
+for($i = 1; $i -lt 20 ; $i ++)
+{
+    echo "line $1"
+}
